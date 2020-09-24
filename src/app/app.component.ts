4024249de701822a3796ea3560172183fc5f8cab
Monica C <tigina18@gmail.com>
2020-09-23 23:35:32 -0500

    Final assignment (no bonuses)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
@@ -25,11 +26,26 @@ export class AppComponent {
             // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
             this.sourceList.push(satellite);
           }
-          
- 
-       }.bind(this));
+          this.displayList = this.sourceList.slice(0);
+      }.bind(this));
     }.bind(this));
- 
- }
+    
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+  }
   
 }
+
+
